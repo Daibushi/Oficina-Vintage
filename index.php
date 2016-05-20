d1742d0d3f147267e9094124b165041b2d721ab6
Jaderson Nascimento <jd_pathfinder@hotmail.com>
2016-05-20 18:25:09 -0300

    nome

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 ﻿	<!DOCTYPE html>
 	<html lang="pt-br">
 		<head>
-			<title>loja da Esquina</title>
+			<title>Oficina Vintage</title>
 			<meta charset="utf-8">
 			<meta http-equiv="X-UA-Compatible" content="IE=edge">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
